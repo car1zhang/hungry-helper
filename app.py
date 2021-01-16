af4b28378d7d0fc564acae1c40e3f90f7930505d
Yelll0 <zhang.carl.x@gmail.com>
2021-01-15 23:36:41 -0500

    Update .gitignore

--- app.py
+++ app.py
@@ -4,5 +4,6 @@ import requests, json
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def main():
     return render_template("home.html", data = requests.get("https://api.spoonacular.com/recipes/complexSearch?query=pasta&maxFat=25&number=2&apiKey=58dec5f444fb4942b7a123310f0eb653").json())
