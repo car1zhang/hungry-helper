8f5a5c25360b6e5e0ea9f84d76138022cf5862be
Aayush Panda <aayush.vinayak@gmail.com>
2021-01-16 12:25:31 -0500

    Fixed api requests

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__)
 
+f_request = ''
+paramstring = ''
 search_url = 'https://api.spoonacular.com/recipes/complexSearch?'
 id_url = "https://api.spoonacular.com/recipes/"
 api_key = '09e6cdabd57e434ca76916051f205189'  # other key: 58dec5f444fb4942b7a123310f0eb653
@@ -28,6 +30,10 @@ params = {
     'number': '1'
 }
 
+for key, value in params.items():
+    if value != '':
+        paramstring += ("&" + key + "=" + value)
+
 
 @app.route("/")
 @app.route("/home")
@@ -40,7 +46,7 @@ def main():
 def post():
     r_ingredients = request.form["ingredients"]
     r_restrictions = request.form["restrictions"]
-    results = requests.get(search_url, params, api_key).json()
+    results = requests.get(search_url + paramstring + "&apiKey=" + api_key).json()
     top_result = results["results"][0]
     top_name = top_result["title"]
     return render_template("home.html", name=top_name, link=top_result)
