4f33014ce409163eb4e9a58d18b6b6307e19ee83
Aayush Panda <aayush.vinayak@gmail.com>
2021-01-16 14:28:20 -0500

    refactored app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 # Api info
 search_url = 'https://api.spoonacular.com/recipes/complexSearch?'
-id_url = "https://api.spoonacular.com/recipes/"
+id_url = 'https://api.spoonacular.com/recipes/'
 
 api_key = '7daff3f13857445097e41523fc01d44a'  # other key: 58dec5f444fb4942b7a123310f0eb653
 
@@ -36,24 +36,24 @@ def get_recipe(params):
     # Removing null params
     for key, value in params.items():
         if value != '':
-            paramstring += ("&" + key + "=" + value)
+            paramstring += ('&' + key + '=' + value)
 
-    results = requests.get(search_url + paramstring + "&apiKey=" + api_key).json()
+    results = requests.get(search_url + paramstring + '&apiKey=' + api_key).json()
 
     return results
 
 
-@app.route("/")
-@app.route("/home")
+@app.route('/')
+@app.route('/home')
 def main():
-    return render_template("home.html", input="")
+    return render_template('home.html', input='')
 
 
-@app.route("/", methods=["POST"])
-@app.route("/home", methods=["POST"])
+@app.route("/", methods=['POST'])
+@app.route("/home", methods=['POST'])
 def post():
-    r_includeIngredients = request.form["ingredients"]
-    r_intolerances = request.form["restrictions"]
+    r_includeIngredients = request.form['ingredients']
+    r_intolerances = request.form['restrictions']
 
     # Updating request params
     params['includeIngredients'] = r_includeIngredients
@@ -62,12 +62,12 @@ def post():
     results = get_recipe(params)
 
     try:
-        top_result = results["results"][0]["title"]
+        top_result = results['results'][0]['title']
 
     except(IndexError):
-        top_result = ['No such recipe exists']
+        top_result = 'Sorry, we do not have a recipe matching your search criteria.'
 
-    return render_template("home.html", name=top_result)
+    return render_template('home.html', name=top_result)
 
 # TODO: Use react to add more ingredients and make the page more interactive
 # TODO: Format results
